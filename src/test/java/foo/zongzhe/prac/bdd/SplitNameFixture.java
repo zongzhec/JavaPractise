5ee742fdea9e8b2abb3189072203afc584332ba9
zongzhec <zongzhe_chen@sina.com>
2019-06-09 22:21:58 +0800

    Concordion example
    set up html

---
+++ src/test/java/foo/zongzhe/prac/bdd/SplitNameFixture.java
@@ -0,0 +1,8 @@
+package foo.zongzhe.prac.bdd;
+
+import org.concordion.intergration.junit4.ConcordionRunner;
+import org.junit.runner.RunWith;
+
+@RunWith(ConcordionRunner.class)
+public class SplitNameFixture {
+}
