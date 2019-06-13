8ad6b99d50f83ceec92e15623c01965bc1be1540
zongzhec <zongzhe_chen@sina.com>
2019-06-13 21:23:56 +0800

    BDD
    BDD practise

---
+++ src/test/java/foo/zongzhe/prac/bdd/MailshotsFixture.java
@@ -0,0 +1,9 @@
+package foo.zongzhe.prac.bdd;
+
+import org.concordion.integration.junit4.ConcordionRunner;
+import org.junit.runner.RunWith;
+
+@RunWith(ConcordionRunner.class)
+public class MailshotsFixture {
+
+}
