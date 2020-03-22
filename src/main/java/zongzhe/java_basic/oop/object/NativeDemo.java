9d8ca0b9c37eeb47c226973f5eabbece024ff236
zongzhec <zongzhe_chen@sina.com>
2020-03-22 12:35:26 +0800

    Add Native Demo

---
+++ src/main/java/zongzhe/java_basic/oop/object/NativeDemo.java
@@ -0,0 +1,13 @@
+package zongzhe.java_basic.oop.object;
+
+
+/**
+ * @see <a href="https://www.bilibili.com/video/av83956740?p=220">https://www.bilibili.com/video/av83956740?p=220</a>
+ * Native修饰符，只能修饰方法，说明不是用Java语言实现的方法体，而是用C语言等实现的。
+ * 用C语言实现后，编译为.dll文件，然后又java代码调用。
+ * 比如：Object.registerNatives(), getClass(), hashCode()。
+ * 对于使用者来说没有区别，需要重写时可以重写。
+ *
+ * */
+public class NativeDemo {
+}
