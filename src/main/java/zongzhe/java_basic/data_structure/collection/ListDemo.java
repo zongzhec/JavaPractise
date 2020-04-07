d7552015f9b9dfe9e6d67fef66cece91c9cf6390
zongzhec <zongzhe_chen@sina.com>
2020-04-07 19:06:50 +0800

    Add ListDemo

--- src/main/java/zongzhe/java_basic/data_structure/collection/ListDemo.java
+++ src/main/java/zongzhe/java_basic/data_structure/collection/ListDemo.java
@@ -2,14 +2,21 @@ package zongzhe.java_basic.data_structure.collection;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.ListIterator;
 
 public class ListDemo {
 
     public static void main(String[] args) {
-        checkListFunctions();
+
+        checkAddFunctions(); // 增
+        checkRemoveFunc(); // 删
+        checkQueryFunc(); // 查
+        checkSetFunc(); // 改
+        checkIterFunc(); // 遍历
+
     }
 
-    private static void checkListFunctions() {
+    private static void checkAddFunctions() {
         List list = new ArrayList();
         list.add("one element");
         list.add(0, "another element"); // 在指定位置添加元素
@@ -22,6 +29,62 @@ public class ListDemo {
 
         for (Object o : list) {
             System.out.println("element: " + o);
+        }
+    }
+
+    private static void checkRemoveFunc() {
+        List list = new ArrayList();
+        list.add("one element");
+        list.add("another element");
+
+        list.remove(0);
+
+        for (Object o : list) {
+            System.out.println("element: " + o);
+        }
+    }
+
+    private static void checkSetFunc() {
+        List list = new ArrayList();
+        list.add("one element");
+        list.add("another element");
+
+        list.set(0, "updated element");
+
+        for (Object o : list) {
+            System.out.println("element: " + o);
+        }
+    }
+
+    private static void checkQueryFunc() {
+        List list = new ArrayList();
+        list.add("one element");
+        list.add("another element");
+        list.add("another element");
+
+        System.out.println("value of first index: " + list.get(0));
+        System.out.println("first index of another element: " + list.indexOf("another element"));
+        System.out.println("last index of another element: " + list.lastIndexOf("another element"));
+    }
+
+    private static void checkIterFunc() {
+        List list = new ArrayList();
+        list.add("one element");
+        list.add("another element");
+        list.add("third element");
+
+        ListIterator iter = list.listIterator();
+        // 往后遍历
+        while (iter.hasNext()) {
+            Object next = iter.next();
+            System.out.println(next);
+        }
+
+        // 往前遍历
+        iter = list.listIterator(list.size()); // 看到index，可以意识到我们其实可以从任意位置开始遍历
+        while (iter.hasPrevious()) {
+            Object prev = iter.previous();
+            System.out.println(prev);
         }
     }
 }
