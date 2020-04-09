63228d310b0c88be1920741ca7114b14c2b0284e
zongzhec <zongzhe_chen@sina.com>
2020-04-09 17:32:12 +0800

    Add Thread Demo

--- src/main/java/zongzhe/java_basic/multithread/ThreadDemo.java
+++ src/main/java/zongzhe/java_basic/multithread/ThreadDemo.java
@@ -10,6 +10,18 @@ public class ThreadDemo {
         MyThread thread2 = new MyThread("thread 2");
         thread2.start();
 
+        for (int i = 1; i <= 100; i += 2) {
+            if (i == 5) {
+                try {
+                    thread1.join(); // main 线程被thread1 线程加塞，只能等到thread1结束
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            System.out.println("main - 奇数：" + i);
+        }
+
+
     }
 
 }
